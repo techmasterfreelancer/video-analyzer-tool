5fe3e61f517d265377bbe49ecf85f9ba708ed178
techmasterfreelancer <techmasterfreelancer@gmail.com>
2025-12-29 06:43:05 -0800

    Delete App.jsx

--- App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>Video Analyzer Tool</h1>
-      <p>Yahan apna Gemini ka code paste karo.</p>
-    </div>
-  )
-}
